fff5c0d1a5ad7e5400cd83fbeceb8d1a2a7327bb
pinoles <angel.ismael684@gmail.com>
2025-02-06 15:39:52 -0700

    cambios en la app actualizacion de dependecias y splash

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
 import AppNavigator from "../navigation/AppNavigator";
-
-
-
+import * as SplashScreen from 'expo-splash-screen';
 export default function Page() {
+
+
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 1000);
+  }, []);
+  
   return (
       <AppNavigator />
   );
 }
+
 
